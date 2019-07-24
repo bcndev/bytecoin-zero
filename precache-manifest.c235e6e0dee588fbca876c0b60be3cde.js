eeb6d350023457257d8e207396b5548022252545
Bytecoin Developer <developer@bytecoin.org>
2019-07-24 17:35:53 +0900

    New build

---
+++ precache-manifest.c235e6e0dee588fbca876c0b60be3cde.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79b12c927f94f7e4248a449a7b50435",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9646e76db9817883843",
+    "url": "./static/css/main.b4b90673.chunk.css"
+  },
+  {
+    "revision": "ceb4be67f011916285b4",
+    "url": "./static/js/2.b60cc66c.chunk.js"
+  },
+  {
+    "revision": "a9646e76db9817883843",
+    "url": "./static/js/main.942ec820.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1f67c9d118b38256020d8dc37a4bc3de",
+    "url": "./static/media/Arrow_northeast.1f67c9d1.svg"
+  },
+  {
+    "revision": "9f3f690f727eeca4cbbec73c8572c161",
+    "url": "./static/media/Arrow_southeast.9f3f690f.svg"
+  },
+  {
+    "revision": "781cea01c96bf3e6a64223547484d8a7",
+    "url": "./static/media/Arrow_southwest.781cea01.svg"
+  },
+  {
+    "revision": "5575295b66259f90c5118a1ffc5a5e38",
+    "url": "./static/media/logo.5575295b.svg"
+  }
+]);
